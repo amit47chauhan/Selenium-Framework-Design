da5d2164efde1e0d517b1b15860865192eda1af0
amit47chauhan <chauhanamit.ak47@gmail.com>
2026-03-13 17:13:58 +0530

    Included product validation test to test the selection of product is available or not. Then Created a 'testng' xml file to run all the test cases from a single file

---
+++ src/test/java/amitchauhan/Tests/ErrorValidationsTest.java
@@ -0,0 +1,26 @@
+package amitchauhan.Tests;
+
+import amitchauhan.TestComponents.BaseTest;
+import amitchauhan.pageobjects.CartCatalogue;
+import amitchauhan.pageobjects.ProductCatalogue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ErrorValidationsTest extends BaseTest {
+
+    @Test
+    public void loginErrorValidation(){
+        landingPage.loginApplication("virat.kohli@gmail.com", "Virat");
+        Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.");
+    }
+
+    @Test
+    public void productErrorValidation(){
+        String productName = "ZARA COAT 3";
+        ProductCatalogue productCatalogue = landingPage.loginApplication("test4545@gmail.com", "Test@123");
+        productCatalogue.addProductToCart(productName);
+        CartCatalogue cartCatalogue = productCatalogue.goToCart();
+        Boolean match = cartCatalogue.verifyProductDisplay("ZARA COAT 33");
+        Assert.assertFalse(match);
+    }
+}
