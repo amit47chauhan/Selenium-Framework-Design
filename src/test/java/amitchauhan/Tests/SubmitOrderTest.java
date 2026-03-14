e1ab5e3d01cf8aa2b5d79c0fc818c8e19a23bbe4
amit47chauhan <chauhanamit.ak47@gmail.com>
2026-03-14 16:27:45 +0530

    Implementing Parameterization into tests with TestNg Data Provider

--- src/test/java/amitchauhan/Tests/SubmitOrderTest.java
+++ src/test/java/amitchauhan/Tests/SubmitOrderTest.java
@@ -4,16 +4,16 @@ import amitchauhan.TestComponents.BaseTest;
 import amitchauhan.pageobjects.*;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class SubmitOrderTest extends BaseTest {
-    String productName = "ZARA COAT 3";
     String countryName = "india";
 
-    @Test
-    public void submitOrder() throws IOException {
-        ProductCatalogue productCatalogue = landingPage.loginApplication("virat.kohli@gmail.com", "Virat@123");
+    @Test(dataProvider ="getData", groups = "Purchase")
+    public void submitOrder(String email, String password, String productName) throws IOException {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
         productCatalogue.addProductToCart(productName);
 
         CartCatalogue cartCatalogue = productCatalogue.goToCart();
@@ -29,10 +29,18 @@ public class SubmitOrderTest extends BaseTest {
         Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
     }
 
-    @Test(dependsOnMethods = {"submitOrder"})
-    public void orderHistoryCheck(){
-        ProductCatalogue productCatalogue = landingPage.loginApplication("virat.kohli@gmail.com", "Virat@123");
+    @Test(dependsOnMethods = {"submitOrder"}, dataProvider = "getData")
+    public void orderHistoryCheck(String email, String password, String productName){
+        ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
         OrderPage orderPage = productCatalogue.goToOrder();
         Assert.assertTrue(orderPage.verifyOrderDisplay(productName));
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        return new Object[][] {
+                {"virat.kohli@gmail.com","Virat@123","ZARA COAT 3"},
+                {"test4545@gmail.com", "Test@123", "ADIDAS ORIGINAL"}
+        };
     }
 }
