252d167e4ab11a191e3bf2b46a83c48b5318ab27
amit47chauhan <chauhanamit.ak47@gmail.com>
2026-03-14 17:15:57 +0530

    Created seperate json file to provide the login data for which we need to implement methods in baseTest to read the json file and convert it into the haspMap to provide the data to tests

--- src/test/java/amitchauhan/TestComponents/BaseTest.java
+++ src/test/java/amitchauhan/TestComponents/BaseTest.java
@@ -1,16 +1,22 @@
 package amitchauhan.TestComponents;
 
 import amitchauhan.pageobjects.LandingPage;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -58,5 +64,23 @@ public class BaseTest {
     @AfterMethod(alwaysRun = true)
     public  void tearDown(){
         driver.close();
+    }
+
+    // Method to get Json Data to Hashmap
+    public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+        // Read json to string
+        String jsonContent = FileUtils.readFileToString(new File( filePath),
+                StandardCharsets.UTF_8);
+
+        // Converting String to HashMap using JACKSON DATABIND
+        // 1- Add Jackson data-bind dependency first
+        // 2-
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<HashMap<String, String>> data = objectMapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
+        });
+
+        return data;
+
+        // returns {map1, map2}
     }
 }
--- src/test/java/amitchauhan/Tests/SubmitOrderTest.java
+++ src/test/java/amitchauhan/Tests/SubmitOrderTest.java
@@ -7,17 +7,19 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class SubmitOrderTest extends BaseTest {
     String countryName = "india";
 
     @Test(dataProvider ="getData", groups = "Purchase")
-    public void submitOrder(String email, String password, String productName) throws IOException {
-        ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
-        productCatalogue.addProductToCart(productName);
+    public void submitOrder(HashMap<String, String> input) throws IOException {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
+        productCatalogue.addProductToCart(input.get("product"));
 
         CartCatalogue cartCatalogue = productCatalogue.goToCart();
-        Boolean match = cartCatalogue.verifyProductDisplay(productName);
+        Boolean match = cartCatalogue.verifyProductDisplay(input.get("product"));
         Assert.assertTrue(match);
 
         CheckoutPage checkoutPage = cartCatalogue.goToCheckout();
@@ -30,17 +32,29 @@ public class SubmitOrderTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = {"submitOrder"}, dataProvider = "getData")
-    public void orderHistoryCheck(String email, String password, String productName){
-        ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
+    public void orderHistoryCheck(HashMap<String, String> input){
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
         OrderPage orderPage = productCatalogue.goToOrder();
-        Assert.assertTrue(orderPage.verifyOrderDisplay(productName));
+        Assert.assertTrue(orderPage.verifyOrderDisplay(input.get("product")));
     }
 
     @DataProvider
-    public Object[][] getData(){
+    public Object[][] getData() throws IOException {
+//        HashMap<String, String> map1 = new HashMap<String, String>();
+//        map1.put("email", "virat.kohli@gmail.com");
+//        map1.put("password", "Virat@123");
+//        map1.put("product", "ZARA COAT 3");
+//
+//        HashMap<String, String> map2 = new HashMap<String, String>();
+//        map2.put("email", "test4545@gmail.com");
+//        map2.put("password", "Test@123");
+//        map2.put("product", "ADIDAS ORIGINAL");
+
+        List<HashMap<String, String>> data = getJsonDataToMap(System.getProperty("user.dir")
+                + "\\src\\main\\java\\amitchauhan\\data\\PurchaseOrder.json");
+
         return new Object[][] {
-                {"virat.kohli@gmail.com","Virat@123","ZARA COAT 3"},
-                {"test4545@gmail.com", "Test@123", "ADIDAS ORIGINAL"}
+                {data.get(0)}, {data.get(1)}
         };
     }
 }
